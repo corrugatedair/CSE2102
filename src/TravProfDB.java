4c8a2f285dfe674c708352e668c2a7ce136ced79
unknown <JJrodny@.(none)>
2009-09-23 22:00:08 -0400

    Revision 1

--- src/TravProfDB.java
+++ src/TravProfDB.java
@@ -1,19 +1,163 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 
 public class TravProfDB {
 
-	public TravProfDB(String test) {
-		// TODO Auto-generated constructor stub
-	}
-	public void insertNewProfile(TravProf test){}
-	public boolean deleteProfile(String test1, String test2){return true;}
-	public TravProf findProfile(String test1, String test2){return new TravProf();}
-	public TravProf findFirstProfile() {return new TravProf();}
-	public TravProf findNextProfile() {return new TravProf();}
-	public void writeAllTravProf(String test1){}
-	public void initializeDataBase(String test){}
+	/**
+	 * @param args
+	 */
+	int numTravelers = 0;
+	TravProf travelerList[];
+	int currentTravelerIndex = 0;
+	String fileName = "";
 	
-	
-	
-	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+	}
+
+	public TravProfDB(String textFile)
+	{
+		fileName = textFile;
+		// Read the text file, saving the data
+		// in it to a local variable
+		
+	}
+	
+	public void insertNewProfile(TravProf myTravProf)
+	{
+		travelerList[travelerList.length] = myTravProf;
+	}
+	
+	public TravProf findProfile(String myTravAgentID, String lastName)
+	{
+		currentTravelerIndex = FindTravProfint(myTravAgentID, lastName);
+		return travelerList[currentTravelerIndex];
+	}
+
+	public TravProf findFirstProfile()
+	{
+		currentTravelerIndex = 0;
+		return travelerList[0];
+	}
+
+	public TravProf findNextProfile()
+	{
+		currentTravelerIndex++;
+		return travelerList[currentTravelerIndex];
+	}
+	
+	public boolean deleteProfile(String myTravAgentID, String lastName)
+	{
+		int myTravProfNum = FindTravProfint(myTravAgentID, lastName);
+		travelerList[myTravProfNum] = null;
+		for (int ii = myTravProfNum+1; ii < travelerList.length; ii++)
+		{
+			travelerList[ii-1] = travelerList[ii];
+		}
+		return false;
+	}
+	
+	public int FindTravProfint(String myTravAgentID, String lastName)
+	{
+		for (int ii = 0; ii < travelerList.length; ii++)
+		{
+			if (travelerList[ii].getLastName().equals(lastName))
+			{
+				if (travelerList[ii].getTravAgentID().equals(myTravAgentID))
+				{
+					return ii;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public TravProf FindTravProf(String myTravAgentID, String lastName)
+	{
+		for (int ii = 0; ii < travelerList.length; ii++)
+		{
+			if (travelerList[ii].getLastName().equals(lastName))
+			{
+				if (travelerList[ii].getTravAgentID().equals(myTravAgentID))
+				{
+					currentTravelerIndex = ii;
+					return travelerList[ii];
+				}
+			}
+		}
+		return null;
+	}
+	
+	public boolean writeAllTravProf()
+	{
+		try {
+			File file = new File(fileName);
+	        BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+	        
+			for (int ii = 0; ii < travelerList.length; ii++)
+			{
+				TravProf myTempTravProf = travelerList[ii];
+				String travAgentID = myTempTravProf.getTravAgentID();
+				String firstName = myTempTravProf.getFirstName();
+				String lastName = myTempTravProf.getLastName();
+				String address = myTempTravProf.getAddress();
+				String phone = myTempTravProf.getPhone();
+				float tripCost = myTempTravProf.getTripCost();
+				String travelType = myTempTravProf.getTravelType();
+				String paymentType = myTempTravProf.getPaymentType();
+				MedCond medCondInfo = myTempTravProf.getMedCondInfo();
+				String mdContact = medCondInfo.getMdContact();
+				String mdPhone = medCondInfo.getMdPhone();
+				String algType = medCondInfo.getAlgType();
+				String illType = medCondInfo.getIllType();
+
+				out.write(travAgentID + ", " + firstName + ", " + lastName + ", " + address
+						+ ", " + phone + ", " + tripCost + ", " + travelType + ", " + paymentType
+						+ ", " + mdContact + ", " + mdPhone + ", " + algType + ", " + illType + "\n");
+
+			}
+	        out.close();
+	    } catch (Exception e) {
+	    }
+
+		return true;
+	}
+	
+	public void initializeDatabase()
+	{
+		try {
+	        BufferedReader in = new BufferedReader(new FileReader("infilename"));
+	        String str;
+	        while ((str = in.readLine()) != null) {
+	            process(str);
+	        }
+	        in.close();
+	    } catch (Exception e) {
+	    }
+
+		
+	}
+
+	private void process(String str) {
+
+		
+		String travAgentID = myTempTravProf.getTravAgentID();
+		String firstName = myTempTravProf.getFirstName();
+		String lastName = myTempTravProf.getLastName();
+		String address = myTempTravProf.getAddress();
+		String phone = myTempTravProf.getPhone();
+		float tripCost = myTempTravProf.getTripCost();
+		String travelType = myTempTravProf.getTravelType();
+		String paymentType = myTempTravProf.getPaymentType();
+		MedCond medCondInfo = myTempTravProf.getMedCondInfo();
+		String mdContact = medCondInfo.getMdContact();
+		String mdPhone = medCondInfo.getMdPhone();
+		String algType = medCondInfo.getAlgType();
+		String illType = medCondInfo.getIllType();
+	}
 }
