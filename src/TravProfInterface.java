196f46e53780ffacdfe343cf80b2c82414c0cb07
Amy Richards <anthropofobe@gmail.com>
2009-09-23 21:52:41 -0400

    Initial Uploads

---
+++ src/TravProfInterface.java
@@ -0,0 +1,21 @@
+
+public class TravProfInterface {
+	String[] menuChoices;
+
+	public TravProfInterface(String fileName) {
+		new TravProfDB(fileName);
+		menuChoices = new String[9];
+		menuChoices[0] = "Create a new traveler profile";
+		menuChoices[1] = "Delete a traveler by Name and Travel Agent ID";
+		menuChoices[2] = "Find and display a Traveler Profile by Name and Travel Agent ID";
+		menuChoices[3] = "Traveler Profile Modifications";
+		menuChoices[4] = "Display all profiles";
+		menuChoices[5] = "Write to Database";
+		menuChoices[6] = "Initialize Database";
+	}
+	public void getUserChoice()
+	{
+		System.out.println()
+	}
+
+}
